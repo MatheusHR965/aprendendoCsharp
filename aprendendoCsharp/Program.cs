9be820a70e9ce89e59b7b6d3fdefadebc7933f6f
matheus.hrbueno <matheus.hrbueno@ITA0557216W10-1>
2024-08-06 16:04:32 -0300

    Adicionar arquivos de projeto.

---
+++ aprendendoCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aprendendoCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ola Mudo!");
+
+            Console.WriteLine(3 + 3);
+
+            Console.Read();
+
+        }
+    }
+}
