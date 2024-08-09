7dcf036a08bfa06a5ff33714f9c4e487408d6a33
matheus <matheus835Hq@gmail.com>
2024-08-09 17:18:25 -0300

    Exercicios completados

---
+++ Cap03_Ex1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap03_Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, R;
+
+            Console.Write("Entre o valor <A>: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Entre o valor <B>: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            if (R > 10)
+                Console.WriteLine("Resultado = {0}", R);
+
+            Console.WriteLine();
+            Console.Write("Tecla <Enter> para encerrar... ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Cap3_Ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap3_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, R;
+
+            Console.Write("Entre o valor <A>: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Entre o valor <B>: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            if (R > 10)
+                Console.WriteLine("Resultado ultrapassa dez.");
+            else
+                Console.WriteLine("Resultado não ultrapassa dez.");
+
+            Console.WriteLine();
+            Console.Write("Tecla <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
